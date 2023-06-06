3108be6108cad97ccf8c5085a0de67169c66f0a2
MenglinC <55335232+MenglinC@users.noreply.github.com>
2023-06-06 14:13:15 +0800

    Rename tumor_normal.R to cellLineage/tumor_normal.R

--- cellLineage/tumor_normal.R
+++ cellLineage/tumor_normal.R
@@ -2,13 +2,13 @@ setwd("F://Rworkplace//coverage")
 coverage<-read.table("coverage.txt",sep = "\t")
 germline<-read.table("germline_mutations_hg19.txt",sep = "\t",header = T)
 somatic<-read.table("somatic_mutations_hg19.txt",sep = "\t",header = T)
-#¿ÉÒÔÀûÓÃmergeº¯ÊýÀ´½â¾öÕâ¼þÊÂÇé
+#å¯ä»¥åˆ©ç”¨mergeå‡½æ•°æ¥è§£å†³è¿™ä»¶äº‹æƒ…
 germline$Tumor_add<-germline$Tumor_ref+germline$Tumor_alt
 germline_sub<-subset(germline,select = c(Chr,Start,Tumor_add))
 germline_sub<-within(germline_sub,Site<- paste(Chr,Start,sep="_"))
 coverage<-within(coverage,Site<-paste(V1,V2,sep="_")) 
-#½«withinºÍwithÅª»ìÁË£¬¾Í²úÉúÁËÃð¶¥Ö®ÔÖ
-#ÓÐÊ±¼äÇø·ÖÒ»ÏÂ
+#å°†withinå’Œwithå¼„æ··äº†ï¼Œå°±äº§ç”Ÿäº†ç­é¡¶ä¹‹ç¾
+#æœ‰æ—¶é—´åŒºåˆ†ä¸€ä¸‹
 germline_sub_coverage<-merge(germline_sub,coverage,by.x = "Site",by.y = "Site")
 all_last<-subset(germline_sub_coverage,select = c(Site,Tumor_add,V4))
 colnames(all_last)<-c("Site","tumor_sum","coverage")
@@ -23,8 +23,8 @@ somatic_sub<-subset(somatic,select = c(Chr,Start,Tumor_add))
 
 somatic_sub<-within(somatic_sub,Site<- paste(Chr,Start,sep="_"))
 coverage<-within(coverage,Site<-paste(V1,V2,sep="_")) 
-#½«withinºÍwithÅª»ìÁË£¬¾Í²úÉúÁËÃð¶¥Ö®ÔÖ
-#ÓÐÊ±¼äÇø·ÖÒ»ÏÂ
+#å°†withinå’Œwithå¼„æ··äº†ï¼Œå°±äº§ç”Ÿäº†ç­é¡¶ä¹‹ç¾
+#æœ‰æ—¶é—´åŒºåˆ†ä¸€ä¸‹
 somatic_sub_coverage<-merge(somatic_sub,coverage,by.x = "Site",by.y = "Site")
 all_last<-subset(somatic_sub_coverage,select = c(Site,Tumor_add,V4))
 colnames(all_last)<-c("Site","tumor_sum","coverage")
